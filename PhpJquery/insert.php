d33245675edbc81d54b7c1e7c4956745d28def31
pramodsaini <pramodsaini25062000@gmail.com>
2023-12-12 01:00:24 -0800

    take the record from the databse

--- PhpJquery/insert.php
+++ PhpJquery/insert.php
@@ -3,8 +3,7 @@ if(isset($_REQUEST["roll"]) && isset($_REQUEST["name"]) && isset($_REQUEST["avg"
     $roll=$_REQUEST["roll"];
     $name=$_REQUEST["name"];
     $avg=$_REQUEST["avg"];
-    echo $roll." ".$name." ".$avg;
-
+ 
     $conn=mysqli_connect("localhost","root","","srev");
     if(mysqli_query($conn,"insert into demo values($roll,'$name',$avg)")>0){
         echo"success";
